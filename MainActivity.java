cd603b5bb2ef6ee2009358a2606bda85a172a5f9
mehdi bagvand <mehdiebagvand@yahoo.com>
2013-03-07 14:31:01 +0330

    hello
    I change 3 file and it is not complet
    I dont know how to complete it

---
+++ MainActivity.java
@@ -0,0 +1,234 @@
+package com.app.calender;
+
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+
+public class MainActivity extends Activity {
+	private ProgressDialog progressBar;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		/*ImageButton imbtn0=(ImageButton) findViewById(R.id.imageButton0);*/
+		ImageButton imbtn1=(ImageButton) findViewById(R.id.imageButton1);
+		ImageButton imbtn2=(ImageButton) findViewById(R.id.imageButton2);
+		ImageButton imbtn3=(ImageButton) findViewById(R.id.imageButton3);
+		ImageButton imbtn4=(ImageButton) findViewById(R.id.imageButton4);
+		ImageButton imbtn5=(ImageButton) findViewById(R.id.imageButton5);
+		ImageButton imbtn6=(ImageButton) findViewById(R.id.imageButton6);
+		ImageButton imbtn7=(ImageButton) findViewById(R.id.imageButton7);
+		ImageButton imbtn8=(ImageButton) findViewById(R.id.imageButton8);
+		ImageButton imbtn9=(ImageButton) findViewById(R.id.imageButton9);
+		ImageButton imbtn10=(ImageButton) findViewById(R.id.imageButton10);
+		ImageButton imbtn11=(ImageButton) findViewById(R.id.imageButton11);
+		ImageButton imbtn12=(ImageButton) findViewById(R.id.imageButton12);
+		ImageButton imbtn13=(ImageButton) findViewById(R.id.imageButton13);
+		ImageButton imbtn14=(ImageButton) findViewById(R.id.imageButton14);
+		ImageButton imbtn15=(ImageButton) findViewById(R.id.imageButton15);
+		ImageButton imbtn16=(ImageButton) findViewById(R.id.imageButton16);
+		ImageButton imbtn17=(ImageButton) findViewById(R.id.imageButton17);
+		ImageButton imbtn18=(ImageButton) findViewById(R.id.imageButton18);
+		ImageButton imbtn19=(ImageButton) findViewById(R.id.imageButton19);
+		ImageButton imbtn20=(ImageButton) findViewById(R.id.imageButton20);
+		ImageButton imbtn21=(ImageButton) findViewById(R.id.imageButton21);
+		ImageButton imbtn22=(ImageButton) findViewById(R.id.imageButton22);
+		ImageButton imbtn23=(ImageButton) findViewById(R.id.imageButton23);
+		ImageButton imbtn24=(ImageButton) findViewById(R.id.imageButton24);
+		ImageButton imbtn25=(ImageButton) findViewById(R.id.imageButton25);
+		ImageButton imbtn26=(ImageButton) findViewById(R.id.imageButton26);
+		ImageButton imbtn27=(ImageButton) findViewById(R.id.imageButton27);
+		ImageButton imbtn28=(ImageButton) findViewById(R.id.imageButton28);
+		ImageButton imbtn29=(ImageButton) findViewById(R.id.imageButton29);
+		ImageButton imbtn30=(ImageButton) findViewById(R.id.imageButton30);
+		ImageButton imbtn31=(ImageButton) findViewById(R.id.imageButton31);
+		
+		///////
+		/*imbtn0.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+			
+			}});*/
+		//
+		
+		imbtn1.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn2.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn3.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn4.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn5.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn6.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn7.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn8.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn9.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn10.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn11.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn12.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn13.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn14.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn15.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn16.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn17.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn18.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn19.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn20.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn21.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn22.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn23.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn24.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn25.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn26.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn27.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn28.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn29.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn30.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		imbtn31.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				other_activity("hello");
+			}});
+		//
+		
+		
+	}
+
+	private void other_activity(String a) {
+		progressBar = ProgressDialog.show(MainActivity.this, "", "");
+		progressBar.setContentView(R.layout.activity2);
+		progressBar.setCancelable(true);
+		}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
